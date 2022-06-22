3d5d8e6270fdfb74ad4bbbe7764ff4faabfa9736
Pedro Queiroz <997556814p@gmail.com>
2022-06-22 12:36:05 -0300

    atividades

---
+++ fundamentos/bloco-04/dia-01/exercicio 1/scripts.js
@@ -0,0 +1,11 @@
+const myName = "Carolina";
+const birthCity = "Itararé";
+let birthyear = 2004;
+birthyear = 2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthyear);
+
+
+
---
+++ fundamentos/bloco-04/dia-01/exercicio 5/scripts.js
@@ -0,0 +1,20 @@
+let prova = "lista"
+
+switch(prova) {
+    case("aprovado"):
+        console.log("parabens")
+        break;
+
+    case("lista"):
+        console.log("lista de espera")
+        break;
+
+    case("reprovado"):
+        console.log("reprovou")
+        break;
+
+    default:
+        console.log("não se aplica");
+
+
+}
