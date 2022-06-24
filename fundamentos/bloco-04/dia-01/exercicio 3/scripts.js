757ab4ac7e3409ff42094bb51ced750262b0389a
Pedro Queiroz <997556814p@gmail.com>
2022-06-24 11:44:38 -0300

    Reorganização das Branchs

--- fundamentos/bloco-04/dia-01/exercicio 3/scripts.js
+++
@@ -1,7 +0,0 @@
-const base = 5;
-const height = 8;
-const area = base * height;
-const perimeter = base + base + height + height;
-
-console.log(area);
-console.log(perimeter);
--- fundamentos/bloco-04/dia-01/exercicio 7/scripts.js
+++
@@ -1,9 +0,0 @@
-const primeroAngulo = 90;
-const segundoAngulo = 45;
-const terceiroAngulo = 45;
-
-if(primeroAngulo + segundoAngulo + terceiroAngulo == 180) {
-    console.log("È um triangulo");
-} else {
-    console.log("Não é um triangulo");
-}
