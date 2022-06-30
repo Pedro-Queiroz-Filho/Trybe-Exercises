b981bc148b284f9c27948af9480bf486f9fd2c1a
Pedro Queiroz <997556814p@gmail.com>
2022-06-30 15:21:05 -0300

    testes

--- Fundamentos/bloco-05/dia-02/exemplo 1/script.js
+++ Fundamentos/bloco-05/dia-02/exemplo 1/script.js
@@ -1,5 +1,16 @@
 // arquivo script.js
+let paragrafo = document.body.firstElementChild.children[1];
+paragrafo.style.backgroundColor = "red"
+function mudaCor(){
+    paragrafo.style.backgroundColor = "blue"
+}
+
+function mudaCor2(){
+    paragrafo.style.backgroundColor = "red"
+}
 
 console.log(document.getElementById('start').nextSibling) // nó
-
 console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
+
+paragrafo.addEventListener("click",mudaCor);
+paragrafo.addEventListener("click",mudaCor2);
