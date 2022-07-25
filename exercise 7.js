9aea332eba8b18e1e1a6eedd3630c300194433d8
Pedro Queiroz <997556814p@gmail.com>
2022-07-25 19:29:25 -0300

    Exercicios do dia

---
+++ exercise 7.js
@@ -0,0 +1,83 @@
+const books = [
+  {
+    id: 1,
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
+    author: {
+      name: "George R. R. Martin",
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
+    author: {
+      name: "J. R. R. Tolkien",
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: "Fundação",
+    genre: "Ficção Científica",
+    author: {
+      name: "Isaac Asimov",
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: "Duna",
+    genre: "Ficção Científica",
+    author: {
+      name: "Frank Herbert",
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: "A Coisa",
+    genre: "Terror",
+    author: {
+      name: "Stephen King",
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
+    author: {
+      name: "H. P. Lovecraft",
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// Adicione o código do exercício aqui:
+
+function authorWith3DotsOnName(arrayBooks) {
+  const arrayNomes = arrayBooks.map((livros) => livros.author.name);
+  let nomesTresPontos = '';
+   arrayNomes.forEach((nomes) => {
+    let pontos = 0;
+    for(let index = 0; index < nomes.length; index += 1){
+        if(nomes[index] === '.') {
+            pontos += 1
+        }
+    }
+    if(pontos === 3) {
+        nomesTresPontos += nomes
+    }
+  })
+  return nomesTresPontos
+}
+
+console.log(authorWith3DotsOnName(books));
